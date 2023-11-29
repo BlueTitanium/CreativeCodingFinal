4b29ab8e95bfa8a5db2fade0351dc05c9f7074d6
Taneim Miah <xyzftw@gmail.com>
2023-11-28 21:42:13 -0500

    buttons

--- sketch.js
+++ sketch.js
@@ -5,14 +5,14 @@ Creative Coding Final
 /*
 Idea:
 - Fishing game 
-    - Start Screen
+    - Start Screen (maybe I'll make the start screen the same as the aquarium)
     - Audio Slider (maybe)
     - Main area is aquarium
         - in the aquarium zone you get to see all of the fishes you caught swimming around
         - Button to move to different areas (this button will be shown in all locations)
     - Different areas
         - you can fish in the various locations
-        - 3 fishes per location
+        - 1-3 fishes per location
         - Regular Lake
         - Magma
         - Clouds (sky)
@@ -83,33 +83,61 @@ class GameFish {
 
 class Button {
     isHovering=false;
-    constructor(x, y, w, h, func){
+    isActive=false;
+    constructor(x, y, w, h, func, text = "hi"){
         this.x = x;
         this.y = y;
         this.w = w;
         this.h = h;
         this.func = func;
+        this.text = text;
+        this.borderSize = 4;
     }
     display(){
         push();
-        
-        if(mouseX > this.x && mouseX < this.x + this.w
+        if(this.isActive){
+            fill("#FF9800");
+            rect(this.x-this.borderSize,
+                this.y-this.borderSize,
+                this.w+this.borderSize*2,
+                this.h+this.borderSize*2, 20);
+            fill("#3FAEC0");
+            rect(this.x,
+                    this.y,
+                    this.w,
+                    this.h, 20);
+        } else if(mouseX > this.x && mouseX < this.x + this.w
             && mouseY > this.y && mouseY < this.y + this.h){
             this.isHovering = true;
+            fill("#FF2667");
+            rect(this.x-this.borderSize,
+                this.y-this.borderSize,
+                this.w+this.borderSize*2,
+                this.h+this.borderSize*2, 20);
             fill("#2667ff");
             rect(this.x,
                 this.y,
                 this.w,
                 this.h, 20);
+            
         } else {
             this.isHovering = false;
+            fill("#00133960");
+            rect(this.x-this.borderSize,
+                this.y-this.borderSize,
+                this.w+this.borderSize*2,
+                this.h+this.borderSize*2, 20);
             fill("#759fff");
             rect(this.x,
                 this.y,
                 this.w,
                 this.h, 20);
+                
         }
-
+        fill("#FFFFFF");
+        textSize(15);
+        textAlign(CENTER, CENTER);
+        text(this.text, this.x+this.w/2, this.y+this.h/2-2);
         if(this.isHovering && mouseIsPressed){
             this.func();
         }
@@ -119,7 +147,7 @@ class Button {
 }
 
 //scenes
-var current_scene = 2; //StartScene, Aquarium, Lake, Volcano, Sky
+var current_scene = 1; //StartScene, Aquarium, Lake, Volcano, Sky
 
 //minigame
 var current_game_state = 0; //no fishing game, fishing game start, fishing game, fishing game end
@@ -135,9 +163,11 @@ var nextY = 0;
 var nextScale = 1;
 
 //button variables
-var sceneSwitchButton;
 var AquariumButton, LakeButton, VolcanoButton, SkyButton;
-
+var buttonWidth = 100;
+var buttonSpacing = 20;
+var startingX = 20;
+var startingY = 20;
 var col = "#030b1f";
 
 //hold all of the fishes in a list
@@ -146,18 +176,33 @@ var lakeFishies = [];
 var volcanoFishies = [];
 var skyFishies = [];
 
+var fnt;//RALEWAY From google fonts
+
+function preload(){
+    fnt = loadFont("assets/Raleway-SemiBold.ttf");
+}
+
 function setup() {
     createCanvas(800, 600);
     noStroke();
-    sceneSwitchButton
-     = new Button(width-20-60,20,60,30,sceneSwitchFunction);
+    textFont(fnt);
+    AquariumButton = new Button(startingX + buttonWidth*0 + buttonSpacing*0,startingY,buttonWidth,30,GoToAquarium, "aquarium");
+    LakeButton = new Button(startingX + buttonWidth*1 + buttonSpacing*1,startingY,buttonWidth,30,GoToLake, "lake");
+    VolcanoButton = new Button(startingX + buttonWidth*2 + buttonSpacing*2,startingY,buttonWidth,30,GoToVolcano, "volcano");
+    SkyButton = new Button(startingX + buttonWidth*3 + buttonSpacing*3,startingY,buttonWidth,30,GoToSky, "clouds");
+
+    AquariumButton.isActive = true;
 }
   
 function draw() {
     background(col);
 
     var hoveringButton = checkIfButtonIsHovered(); //so that an interaction doesn't happen by accident
-    
+    if(hoveringButton){
+        cursor('pointer');
+    } else {
+        cursor(ARROW);
+    }
 
     switch (current_scene) {
         case 0: // START SCENE
@@ -177,7 +222,8 @@ function draw() {
             }
             drawAquariumFG();
             //UI
-            sceneSwitchButton.display();
+            
+            drawUIButtons();
             break;
         case 2: // LAKE
             
@@ -192,21 +238,22 @@ function draw() {
 
 
             //UI
-            sceneSwitchButton.display();
+            
+            drawUIButtons();
             break;
         case 3: // VOLCANO
-
+            drawVolcanoBG();
 
             //UI
-            sceneSwitchButton.display();
             
+            drawUIButtons();
             break;
         case 4: // SKY
-
+            drawSkyBG();
 
             //UI
-            sceneSwitchButton.display();
             
+            drawUIButtons();
             break;
         default:
             break;
@@ -231,21 +278,88 @@ function draw() {
 
 }
 
+function drawUIButtons(){
+    push();
+    fill("#181e36");
+    rect(-25,-15,width*1.5,90,20);
+    fill("#494e73");
+    rect(-20,-20,startingX+20+buttonWidth*4+buttonSpacing*4,90,20);
+    AquariumButton.display();
+    LakeButton.display();
+    VolcanoButton.display();
+    SkyButton.display();
+
+    
+    textAlign(LEFT,BOTTOM);
+    textSize(34);
+    fill("#31485a");
+    text("travel locations",startingX+22+buttonWidth*4+buttonSpacing*4,70/2+15);
+    fill("#FFFFFF");
+    text("travel locations",startingX+20+buttonWidth*4+buttonSpacing*4,70/2 +17);
+    textAlign(LEFT,TOP);
+    textSize(15);
+    fill("#ffffffa1");
+    text("click a button on the left to travel",startingX+20+buttonWidth*4+buttonSpacing*4,70/2+14);
+
+    if(AquariumButton.isActive){
+        fill("#ffffff41");
+        textAlign(RIGHT,TOP);
+        textSize(12);
+        text("you can fish in the lake, volcano, and clouds!",startingX-20+buttonWidth*4+buttonSpacing*4,70/2+19);
+    }
+    pop();
+}
+
 function checkIfButtonIsHovered(){ //checks if any button is hovered
-    return sceneSwitchButton.isHovering 
-        || AquariumButton.isHovering 
+    return AquariumButton.isHovering 
         || LakeButton.isHovering 
         || VolcanoButton.isHovering 
         || SkyButton.isHovering;
 }
-
+function disableAllButtonActive(){
+    AquariumButton.isActive=false;
+    LakeButton.isActive=false;
+    VolcanoButton.isActive=false;
+    SkyButton.isActive=false;
+    AquariumButton.isHovering=false;
+    LakeButton.isHovering=false;
+    VolcanoButton.isHovering=false;
+    SkyButton.isHovering=false;
+    cursor(ARROW);
+}
 function sceneSwitchFunction(){
 
 }
 
+function GoToAquarium(){
+    disableAllButtonActive()
+    AquariumButton.isActive = true;
+    current_scene = 1;
+}
+function GoToLake(){
+    disableAllButtonActive()
+    LakeButton.isActive = true;
+    current_scene = 2;
+}
+function GoToVolcano(){
+    disableAllButtonActive()
+    VolcanoButton.isActive = true;
+    current_scene = 3;
+}
+function GoToSky(){
+    disableAllButtonActive()
+    SkyButton.isActive = true;
+    current_scene = 4;
+}
+
+
 function drawAquariumBG(){
     //function to draw the aquarium background
-    background()
+    background("#001339");
+    fill("#020b11");
+    rect(0,220,width,height);
+    fill("#175982");
+    rect(10,230,width-20,height-230-10,20);
 }
 function drawAquariumFG(){
     //function to draw the aquarium foreground
@@ -256,9 +370,23 @@ function drawFisher(){
 }
 
 function drawLakeBG(){
-
+    background("#87CEEB");
 }
 function drawLakeFG(){
+
+}
+
+function drawVolcanoBG(){
+    background("#36130d");
+}
+function drawVolcanoFG(){
+
+}
+
+function drawSkyBG(){
+    background("#d5fdf4");
+}
+function drawSkyFG(){
 
 }
 
